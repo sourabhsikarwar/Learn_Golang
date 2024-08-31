d42d48056c2c8e0b39fcb176d479f49c2dd39fa6
Sourabh Sikarwar <sourabh.sikarwar@indmoney.com>
2024-08-31 12:46:20 +0530

    Variables

---
+++ 02variables/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+// Only allowed method for variable decl. outside block or method
+var globalVar string = "Hello World"
+
+// Not allowed (Only allowed inside methods)
+// globalVar := "Hello World"
+
+// -----------
+
+// with Capital letter at first position in the variable name,
+// this variable is now public and all the files can access it
+const DummyToken = "DummyToken"
+
+func main() {
+	var username string = "Sourabh"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var num int = 65
+	fmt.Println(num)
+	fmt.Printf("Variable is of type: %T \n", num)
+
+	var floatNum float32 = 65.56758484848
+	fmt.Println(floatNum)
+	fmt.Printf("Variable is of type: %T \n", floatNum)
+
+	var largeFloatNum float64 = 65.567584848487585588
+	fmt.Println(largeFloatNum)
+	fmt.Printf("Variable is of type: %T \n", largeFloatNum)
+
+	// Default values
+	var num1 int
+	fmt.Println(num1)
+	fmt.Printf("Variable is of type: %T \n", num1)
+
+	var str1 string
+	fmt.Println(str1)
+	fmt.Printf("Variable is of type: %T \n", str1)
+
+	// implicit declaration
+	var str2 = "Hello"
+	fmt.Println(str2)
+	fmt.Printf("Variable is of type: %T \n", str2)
+
+	// no var keyword
+	str3 := "Hello with No Var Keyword"
+	fmt.Println(str3)
+	fmt.Printf("Variable is of type: %T \n", str3)
+
+	fmt.Println(globalVar)
+
+	fmt.Println(DummyToken)
+	fmt.Printf("Variable is of type: %T \n", DummyToken)
+}
