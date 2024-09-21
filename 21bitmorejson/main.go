97c932b28dff4f021bd2546e9cd3fa9531ef062e
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2024-09-21 20:42:49 +0530

    decoding json

--- 21bitmorejson/main.go
+++ 21bitmorejson/main.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("Welcome to json in golang")
-	encodeJson()
+	// encodeJson()
+	decodeJson()
 }
 
 func encodeJson() {
@@ -28,4 +29,39 @@ func encodeJson() {
 	finalJson, _ := json.MarshalIndent(myCourse, "", "\t") // 2nd params is prefix and 3rd is indentation
 
 	fmt.Printf("%s\n", finalJson)
+}
+
+func decodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"website": "LearnCodeOnline.in",
+		"tags": ["web-dev", "js"]
+	}
+	`)
+
+	isValidJson := json.Valid(jsonDataFromWeb)
+
+	if isValidJson {
+		fmt.Println("Json is valid")
+		var myCourse course
+
+		// decoding the json into a struct
+		checkValid := json.Unmarshal(jsonDataFromWeb, &myCourse) // 2nd param is a pointer
+
+		if checkValid == nil {
+			fmt.Printf("%#v\n", myCourse) // %#v is used to print the struct in a readable format
+		}
+	} else {
+		fmt.Println("Json is not valid")
+	}
+
+	var myOnlineClass map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineClass)
+	fmt.Printf("%#v\n", myOnlineClass)
+
+	for k, v := range myOnlineClass {
+		fmt.Printf("Key is %v and value is %v and type is %T\n", k, v, v)
+	}
 }
